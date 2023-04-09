e8e9440a87cedd92b1c63943cc02af22e67bf795
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-09 10:49:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var fs = require('fs');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+
+app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname})
 })
+
+app.use(function(req, res) {
+    res.status(404).send('Bruh... 404...');
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

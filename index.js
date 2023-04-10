50ff182ce15fae5aeb6db143dd3f010929ad7d30
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-10 00:41:36 -0400

    e

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 
-const express = require('express')
+const express = require('express');
+const { response } = require('express');
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname+'/static'))
 
-app.get("/", (req, res) => res.type('html').sendFile('/algs/oll.html', {root: __dirname}));
+app.get("/", (req, res) => {
+    res.type('html').sendFile('index.html', {root: __dirname});
+})
+
+app.get("/cs", (req, res) => {
+    res.type('html').sendFile('static/cs.html', {root: __dirname});
+})
 
 app.use(function(req, res) {
     res.status(404).send('Bruh... 404...');

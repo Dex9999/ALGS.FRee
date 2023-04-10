1556bbd5c772e697c8959ad99a483a795ad278c0
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-10 12:57:28 -0400

    Update sets.js

--- static/sets.js
+++ static/sets.js
@@ -1,25 +1,35 @@
-module.exports = (req, res) => {
-    if(req.url === '/oll') {
-        modifyHtml('oll', res)
-    } else if(req.url === '/pll') {
-        modifyHtml('pll', res)
-    } else if(req.url === '/cs') {  
-        modifyHtml('cs', res)
-    } else {
-        res.status(404).send(req.url);
-    }
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import newLineStream from 'new-line';
 
-    function modifyHtml(set, res) {
-    parser._transform = function(data, encoding, done) {
+export default function handler(req, res) {
+  if (req.url === '/oll') {
+    modifyHtml('oll', res);
+  } else if (req.url === '/pll') {
+    modifyHtml('pll', res);
+  } else if (req.url === '/cs') {
+    modifyHtml('cs', res);
+  } else {
+    res.status(404).sendFile('static/404.html', { root: __dirname });
+  }
+
+  async function modifyHtml(set, res) {
+    const pipelineAsync = promisify(pipeline);
+    const parser = new Transform({
+      transform: function (data, encoding, done) {
         let str = data.toString();
-        str = str.replace('setJson', [set]); 
+        str = str.replace('setJson', [set]);
         this.push(str);
         done();
-      };
-    fs
-      .createReadStream('static/set.html')
-      .pipe(newLineStream())
-      .pipe(parser)
-      .pipe(res);
+      },
+    });
+
+    await pipelineAsync(
+      createReadStream('static/set.html'),
+      newLineStream(),
+      parser,
+      res
+    );
   }
 }

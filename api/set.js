2e60c807258b2a30ef251cd5b4078ca657d84477
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-10 13:01:41 -0400

    Create set.js

---
+++ api/set.js
@@ -0,0 +1,35 @@
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import newLineStream from 'new-line';
+
+export default function handler(req, res) {
+  if (req.url === '/oll') {
+    modifyHtml('oll', res);
+  } else if (req.url === '/pll') {
+    modifyHtml('pll', res);
+  } else if (req.url === '/cs') {
+    modifyHtml('cs', res);
+  } else {
+    res.status(404).sendFile('static/404.html', { root: __dirname });
+  }
+
+  async function modifyHtml(set, res) {
+    const pipelineAsync = promisify(pipeline);
+    const parser = new Transform({
+      transform: function (data, encoding, done) {
+        let str = data.toString();
+        str = str.replace('setJson', [set]);
+        this.push(str);
+        done();
+      },
+    });
+
+    await pipelineAsync(
+      createReadStream('static/set.html'),
+      newLineStream(),
+      parser,
+      res
+    );
+  }
+}

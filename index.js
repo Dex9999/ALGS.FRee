2739f67b2967179f3d9eb627dcf84c8736bb825a
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-09 23:40:36 -0400

    e

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-// app.use(express.static(__dirname))
+app.use(express.static(__dirname))
 
-app.get("/", (req, res) => res.type('html').sendFile('algs/new.html', {root: __dirname}));
+app.get("/", (req, res) => res.type('html').sendFile('/algs/oll.html', {root: __dirname}));
 
 app.use(function(req, res) {
     res.status(404).send('Bruh... 404...');

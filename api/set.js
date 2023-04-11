6498b0e0fd6053acc9bdb4131ee75390a2e8ef51
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-11 09:37:57 -0400

    Update set.js

--- api/set.js
+++ api/set.js
@@ -1,27 +1,27 @@
-import { createReadStream } from 'fs';
+import { createReadStream, Transform } from 'fs';
 import { pipeline } from 'stream';
 import { promisify } from 'util';
 import newLineStream from 'new-line';
 
 module.exports = async (req, res) => {
-	try {
-		
-		// Strip leading slash from request path
-		const url = req.url.replace(/^\/+/, '')
+  try {
+    // Strip leading slash from request path
+    const url = req.url.replace(/^\/+/, '')
 
-		console.log(`Url: ${ url }`)
-		const html = modifyHtml(url, res);
+    console.log(`Url: ${ url }`)
+    const html = await modifyHtml(url, res);
 
-		if (!html) return res.status(400).send('Error: could not get page')
+    if (!html) return res.status(400).send('Error: could not get page')
 
-	} catch (err) {
-		if (err.message === 'Protocol error (Page.navigate): Cannot navigate to invalid URL')
-			return res.status(404).end()
+  } catch (err) {
+    if (err.message === 'Protocol error (Page.navigate): Cannot navigate to invalid URL')
+      return res.status(404).end()
 
-		console.error(err)
-		res.status(500).send('Error: Please try again.')
-	}
-    async function modifyHtml(set, res) {
+    console.error(err)
+    res.status(500).send('Error: Please try again.')
+  }
+
+  function modifyHtml(set, res) {
     const pipelineAsync = promisify(pipeline);
     const parser = new Transform({
       transform: function (data, encoding, done) {
@@ -32,11 +32,12 @@ module.exports = async (req, res) => {
       },
     });
 
-    await pipelineAsync(
+    res.setHeader('Content-Type', 'text/html');
+
+    return pipelineAsync(
       createReadStream('../static/set.html'),
       newLineStream(),
       parser,
-      res.setHeader('Content-Type', 'text/html'),
       res
     );
   }

8445519d79758ac26fcefb2f49638d7c5f29ffa7
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-04-11 14:19:48 -0400

    Update set.js

--- api/set.js
+++ api/set.js
@@ -1,46 +1,26 @@
-import { createReadStream } from 'fs';
-import { pipeline } from 'stream/promises';
-import { Transform } from 'stream';
-import newLineStream from 'new-line';
+import { createReadStream, readFile } from 'fs';
 import { promisify } from 'util';
 
-export default async (req, res) => {
-  try {
-    // Strip leading slash from request path
-    const url = req.url.replace(/^\/+/, '');
-
-    console.log(`Url: ${url}`);
-
-    await modifyHtml(url, res);
-
-  } catch (err) {
-    if (err.message === 'Protocol error (Page.navigate): Cannot navigate to invalid URL') {
-      return res.status(404).end();
-    }
-
-    console.error(err);
-    res.status(500).send('Error: Please try again.');
+export default async function handler(req, res) {
+  if (req.url === '/oll') {
+    await modifyHtml('oll', res);
+  } else if (req.url === '/pll') {
+    await modifyHtml('pll', res);
+  } else if (req.url === '/cs') {
+    await modifyHtml('cs', res);
+  } else {
+    res.status(404).sendFile('static/404.html', { root: __dirname });
   }
+}
 
-  async function modifyHtml(set, res) {
-    const pipelineAsync = promisify(pipeline);
-
-    const parser = new Transform({
-      transform: function (data, encoding, done) {
-        let str = data.toString();
-        str = str.replace('setJson', set);
-        this.push(str);
-        done();
-      },
-    });
-
-    res.setHeader('Content-Type', 'text/html');
-
-    await pipelineAsync(
-      createReadStream('../static/set.html'),
-      newLineStream(),
-      parser,
-      res
-    );
-  }
-};
+/**
+ * Modifies the HTML file with the given set
+ * @param {string} set The set to modify the HTML with
+ * @param {Response} res The response object
+ */
+async function modifyHtml(set, res) {
+  const pipelineAsync = promisify(pipeline);
+  const html = await readFile('../static/set.html');
+  const modifiedHtml = html.replace('setJson', [set]);
+  res.send(modifiedHtml);
+}

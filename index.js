76fdb9a82008bac33ab7953fbb89f881226e590e
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-05-10 15:18:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ async function checkPageForLink(req, res) {
     console.log(replacedHtml)
     res.send(replacedHtml);
   }
-  if (url.startsWith('s ' || url.startsWith('search '))){
+  //if (url.startsWith('s ' || url.startsWith('search '))){
       res.send(url.baseUrl)
-  }
+  //}
 }
 
 // Logging

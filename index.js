3417431b07d6ab1796fbd6fee5eb29b66ccf95de
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-05-11 09:17:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-import searchWca from 'definitions.js'
+const searchWca = require('./definitions.js')
 
 //dotenv?!?!?? How am i missing this
 require('dotenv').config()

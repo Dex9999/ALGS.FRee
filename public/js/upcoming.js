871502dbf66db5c44ed32229f36fb85640fb93cf
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-05-28 22:55:23 -0400

    Update upcoming.js

--- public/js/upcoming.js
+++ public/js/upcoming.js
@@ -1,3 +1,119 @@
+const wcaIdInput = document.getElementById('wcaIdInput');
+const wcaIdList = document.getElementById('wcaIdList');
+const dropdownList = document.getElementById('dropdown-list');
+
+document.addEventListener('click', (event) => {
+  if(event.target == document.body){
+    hideDropdown();
+  }
+});
+
+wcaIdInput.addEventListener('input', () => {
+  const inputValue = encodeURIComponent(wcaIdInput.value.toLowerCase());
+
+//  dropdownList.addEventListener('click', (event) => {
+//   const clickedElement = event.target;
+//   console.log(clickedElement.value); 
+// });
+
+fetch(`https://www.worldcubeassociation.org/api/v0/search/users?q=${inputValue}&persons_table=true`)
+    .then(response => response.json())
+    .then(data => {
+      let top3 = [];
+      for (let i = 0; i < 3 && i < data.result.length; i++) {
+        top3.push({
+          name: data.result[i].name,
+          id: data.result[i].id,
+          avatarUrl: data.result[i].avatar.url
+        });
+      }
+      populateOptions(top3);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+// wcaIdList.addEventListener('change', (event) => {
+//   wcaIdInput.value = event.target.value;
+// });
+// document.addEventListener('click', (event) => {
+//   const clickedElement = event.target;
+//   if (dropdownList.contains(clickedElement)) {
+//     console.log(clickedElement);
+//   }
+// });
+
+function populateOptions(options) {
+  dropdownList.innerHTML = '';
+
+  if (options.length > 1) {
+    options.reverse().forEach(option => {
+      const optionElement = document.createElement('li');
+      optionElement.value = option.id;
+      optionElement.innerHTML = `
+        <img src="${option.avatarUrl}" alt="Avatar" width="40" height="40">
+        <span class="bigger-text">${option.name}</span>
+        <span class="smaller-text">${option.id}</span>
+      `;
+      optionElement.addEventListener('click', () => {
+        console.log('yo what?');
+        console.log(optionElement); 
+        const spanElements = optionElement.querySelectorAll('span');
+        const idValue = spanElements[1].innerHTML;
+
+        console.log(idValue);
+        wcaIdInput.value = idValue;
+        hideDropdown();
+        submitWcaId();
+      });
+      dropdownList.appendChild(optionElement);
+    });
+
+    dropdownList.style.display = 'block';
+    dropdownList.style.top = `${wcaIdInput.offsetTop + wcaIdInput.offsetHeight}px`;
+    dropdownList.style.left = `${wcaIdInput.offsetLeft}px`;
+  } else if (options.length === 1) {
+    const optionElement = document.createElement('li');
+    optionElement.value = options[0].id;
+    optionElement.innerHTML = `
+      <img src="${options[0].avatarUrl}" alt="Avatar" width="40" height="40">
+      <span class="bigger-text">${options[0].name}</span>
+    `;
+    console.log(optionElement);
+    optionElement.addEventListener('click', () => {
+      console.log("click moment!");
+      console.log('Clicked option:', optionElement); // New console log
+      wcaIdInput.value = optionElement.value;
+      hideDropdown();
+      submitWcaId();
+    });
+    dropdownList.appendChild(optionElement);
+
+    // Show and position the dropdown list
+    dropdownList.style.display = 'block';
+    dropdownList.style.top = `${wcaIdInput.offsetTop + wcaIdInput.offsetHeight}px`;
+    dropdownList.style.left = `${wcaIdInput.offsetLeft}px`;
+  } else {
+    hideDropdown();
+  }
+}
+function hideDropdown() {
+  dropdownList.style.display = 'none';
+}
+
+wcaIdInput.addEventListener('focus', () => {
+  if (wcaIdInput.value.trim() !== '') {
+    dropdownList.style.display = 'block';
+  }
+});
+
+// wcaIdInput.addEventListener('blur', () => {
+//   hideDropdown();
+// });
+
+
+//unimportant stuff, for actually generating the table after submitWcaId() is run
 const bgColor = '#2c2c2c';
 const txtColor = '#ffffff';
 const accentColor = '#a439d1';
@@ -11,7 +127,6 @@ submitBtn.addEventListener('click', () => {
     submitWcaId();
 });
 
-const wcaIdInput = document.getElementById('wcaIdInput');
 wcaIdInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         submitWcaId();
@@ -22,6 +137,7 @@ let token = null;
 let isTokenFetching = false;
 
 function submitWcaId() {
+  // console.log('button pressed')
     const wcaId = wcaIdInput.value.trim();
     if (wcaId !== '') {
         showLoading();
@@ -29,13 +145,13 @@ function submitWcaId() {
         if (token) {
             fetchCompetitions(wcaId);
         } else {
-            // fetchAndSetToken()
-            //     .then(() => fetchCompetitions(wcaId))
-            //     .catch(error => {
+            fetchAndSetToken()
+                .then(() => fetchCompetitions(wcaId))
+                .catch(error => {
                     console.log('Failed to fetch token:', error);
                     hideLoading();
                     showError('Failed to fetch token. Please try again.');
-                // });
+                });
         }
     } else {
         showError('Please enter a valid WCA ID.');
@@ -99,9 +215,9 @@ async function fetchCompetitions(wcaId) {
 
     try {
         const response = await fetch("https://statistics-api.worldcubeassociation.org/database/query", requestOptions);
-        console.log(response);
+        // console.log(response);
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
 
         loadingDiv.style.display = 'none';
 
@@ -219,3 +335,4 @@ function showError(message) {
 function hideError() {
     errorDiv.style.display = 'none';
 }
+

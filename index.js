92af88f3bad5e314a80694fdbadc832bdf788935
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-06-16 16:26:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -113,63 +113,6 @@ app.use(function (req, res) {
 setInterval(sendMessage, 60000);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
-async function sendMessage() {
-  const query = `query {
-  recentRecords{
-    attemptResult
-    id
-    result{
-      average
-      best
-      averageRecordTag
-      singleRecordTag
-      id
-      person{
-        name
-        country{
-          iso2
-          name
-        }
-        avatar{thumbUrl}
-				wcaId
-      }
-      attempts{
-        result
-      }
-      round{
-        competitionEvent{
-          competition{name id wcaId}
-        }
-      }
-    }
-    tag
-    type
-  }
-}`
-  const webhookURL = 'https://discord.com/api/webhooks/1119265675366645790/RiMtxwxVvyAnsjx9cT8fz0WWrPvcCWG_wuazzJ6XNT916EGH9pX02k7-3g4oP_HBKcsS';
-  let records = await fetch('https://live.worldcubeassociation.org/api/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query: query }),
-    });
-  let res = await records.json()
-  try {
-    await fetch(webhookURL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ content: res }),
-    });
-    console.log('Message sent successfully!');
-  } catch (error) {
-    console.error('Error sending message:', error);
-  }
-}
-
-
 
 async function fetchFromMongoDB(set) {
   // Mongoose stuff

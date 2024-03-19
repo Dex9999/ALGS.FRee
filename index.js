b306ef7195f779fe841d3672f7f3a3e5806659a8
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2024-03-18 21:14:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var cheerio = require('cheerio');
 // Puppeteer stuff
 let chrome = {};
 let puppeteer;
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require('chrome-aws-lambda');
+if (process.env.NODE_ENV === 'production') {
+  chrome = require('@sparticuz/chromium');
   puppeteer = require('puppeteer-core');
 } else {
   puppeteer = require('puppeteer');
@@ -133,7 +133,7 @@ async function getUpcomingCompetitions(res, req) {
   };
   let options = {};
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  if (process.env.NODE_ENV === 'production') {
     options = {
       args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
       defaultViewport: chrome.defaultViewport,
